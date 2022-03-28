7a7a2b89936c477704c369410d69a6c5961f96b1
Amadeusz Annissimo <annissimo@gmail.com>
2022-03-28 10:45:41 +0100

    complete lesson 14

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@
 Rails.application.routes.draw do
   get "about-us", to: "about#index", as: "about"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 end
